9cc10306ca113512e82a8a199a8543abc7f6be77
rimgeurts <geurts_rim@yahoo.co.uk>
2019-11-16 17:22:50 +0000

    inital commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './search-box.styles.css'
+
+const SearchBox = (props) => {
+    return (
+        <input className='search'
+            type='search' 
+            placeholder={props.placeholder} 
+            onChange={props.search} >
+        </input>
+    )
+}
+
+export default SearchBox
