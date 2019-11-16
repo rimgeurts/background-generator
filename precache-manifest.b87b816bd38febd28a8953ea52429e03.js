470ad4ac6f24be7a5220ba4c0de16ca0a177053e
rimgeurts <geurts_rim@yahoo.co.uk>
2019-11-16 18:09:12 +0000

    Updates

---
+++ precache-manifest.b87b816bd38febd28a8953ea52429e03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2c3e95017ce4a9cc7210276a77e067",
+    "url": "/background-generator/index.html"
+  },
+  {
+    "revision": "4583e9daa00708ad6e23",
+    "url": "/background-generator/static/css/main.c99e092d.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/background-generator/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "4583e9daa00708ad6e23",
+    "url": "/background-generator/static/js/main.31b61e2c.chunk.js"
+  },
+  {
+    "revision": "1ebfab59f95e29a4dcf8",
+    "url": "/background-generator/static/js/runtime-main.4baad8dd.js"
+  }
+]);
